3c6595785659030d6b6513b69265af482f6b225e
escandon100 <olakunle23odunade@gmail.com>
2025-08-29 13:47:39 +0100

    update

--- src/lib/cards.js
+++ src/lib/cards.js
@@ -1,14 +1,26 @@
-const card = [
+const card = [  
     {
-        id : 1,
-        name : "Amazon",
-        img : "amazon.webp",
+        id : 4,
+        name : "Apple Itunes",
+        img: "itunes.jpeg",
         cvv : false
     },
-    {   
-        id : 2,
-        name  : "ebay",
-        img : "ebay.png",
+        {
+        id : 9,
+        name : "Razer Gold",
+        img: "Razer-gold.webp",
+        cvv : false
+    },
+        {
+        id : 11 ,
+        name : "Nordstrom",
+        img : "nordstorm.jpg",
+        cvv : false
+    },
+     {
+        id : 13,
+        name : "X-Box",
+        img : "xbox.jpg",
         cvv : false
     },
     {
@@ -18,11 +30,11 @@ const card = [
         cvv : false
 
     },
-    {
-        id : 4,
-        name : "Apple Itunes",
-        img: "itunes.jpeg",
-        cvv : false
+        {
+        id : 12,
+        name : "Sephora",
+        img : "sephora.webp",
+        pin : true,
     },
     {
         id : 5,
@@ -31,66 +43,23 @@ const card = [
         cvv : false
 
     },
-    // {
-    //     id : 6,
-    //     name : "Vanilla ",
-    //     img: "vanilla.webp",
-    //     cvv : true
-    // },
-    // {    
-    //     id : 7,
-    //     name : "American Express",
-    //     img : "american-express.jpg",
-    // },
-    {
-        id : 8,
-        name : "US PSN ",
-        img: "play-station-card.png",
-        cvv : false
-    },
-    {
-        id : 9,
-        name : "Razer Gold",
-        img: "Razer-gold.webp",
-        cvv : false
-    },
+  
+
+
+
+
     {
         id : 10 ,
         name : "Spotify",
         img : "sportify.png",
         cvv : false
     },
-    {
-        id : 11 ,
-        name : "Nordstrom",
-        img : "nordstorm.jpg",
+        {
+        id : 8,
+        name : "US PSN ",
+        img: "play-station-card.png",
         cvv : false
-    },
-    {
-        id : 12,
-        name : "Sephora",
-        img : "sephora.webp",
-        pin : true,
-    },
-    {
-        id : 13,
-        name : "Hulu",
-        img : "hulu.webp",
-        cvv : false
-    },
-    // {
-    //     id : 14,
-    //     name : "Visa Gift Card",
-    //     img : "visa.jpg.jpeg",
-    //     cvv : true
-    // },
-    {
-        id : 15,
-        name : "Paramount+",
-        img : "paramount-card.jpeg",
-        cvv : false
-    },
-
+    }
 ]
 
 export default card
